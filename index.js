3c0399e1439980d21686e502849eb2d0417bcdbc
aidadieudonne <aidaharing10@gmail.com>
2024-12-03 20:34:03 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(num) {
+ if (num <= 400) {
+    return 'This one is on me!'
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  } else if (num >= 400 && num <= 2000){
+     return 'That will be twenty bucks.'
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+} else if (num > 2000 && num < 2500) {
+    return 'I will gladly take your thirty bucks.'
+ 
+} else 
+    return 'No can do.'
+};
+ 
+
+function ternaryCheckCity(city){
+ return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+};
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous' :
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  };
+
+};
